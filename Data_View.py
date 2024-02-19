0d18932c2e2fad2bf430e6a4c83ab18de414c05a
otajir <160440050+otajir@users.noreply.github.com>
2024-02-19 01:23:24 -0500

    Update Data_View.py

--- Data_View.py
+++ Data_View.py
@@ -73,6 +73,8 @@ if 'df' in st.session_state:
 
                 if st.checkbox(f"Utiliser log({column}) pour l'axe Y"):
                     try:
+                        # Convertir les données en types numériques si nécessaire
+                        df_visualize[column] = pd.to_numeric(df_visualize[column], errors='coerce')
                         # Vérifier si toutes les valeurs de la colonne sont positives
                         if (df_visualize[column] > 0).all():
                             fig.add_trace(go.Scatter(x=df_visualize[x_column], y=np.log(df_visualize[column]), mode='lines',
